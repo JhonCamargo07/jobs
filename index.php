c2ff0ceea2cc0345455e724ebd74b7fce4412430
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-09-21 18:15:49 -0500

    Se ajusto los estilos del index, se implemento bootstrap

--- index.php
+++ index.php
@@ -1,63 +1,70 @@
 <?php
-    require_once('controladores/validacion.php');
+require_once('controladores/validacion.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="vistas/css/autostyle.css">
     <link rel="stylesheet" href="vistas/css/style.css">
     <link rel="icon" href="vistas/imagenes/icon-jobs.png">
-    <title>Clases - ADSI</title>
-    <script src="https://kit.fontawesome.com/dca352768f.js" crossorigin="anonymous"></script>
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <script src="https://kit.fontawesome.com/dca352768f.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    <div class="">
-
-        <div class="contenido margin-vertical">
-            
-            <div class="div-izquierda">
-                <div class="text-center"><img src="vistas/imagenes/task.png" class="logo text-center" alt="Logo jobs"></div>
-                <p><strong>Task</strong> te ayuda a recordar las actividades que tienes que hacer diariamente.</p>
-            </div>
-
-            <div class="div-derecha card">
-
-                <form action="vistas/index.php" method="POST" id="formulario">
-
-                    <div class="form-control margin-top">
-                        <input type="text" name="usuario" id="usuario" class="input height-grande" maxlength="30" value="<?php if(isset($usuario)){echo $usuario;} ?>" placeholder="Correo electr&#243;nico" autofocus>
-                        <div><p class="mensaje__input-error" id="mensaje-error">El correo es obligatorio</p></div>
-                    </div>
-
-                    <div class="form-control">
-                        <input type="password" name="password" id="password" class="input height-grande" maxlength="30" value="<?php if(isset($password)){echo $password;} ?>" placeholder="Contraseña">
-                        <i class="eye fas fa-eye-slash" id="icono"></i>
-                        <div><p class="mensaje__input-error" id="mensaje-error2">La contrase&#241;a es obligatoria</p></div>
-                    </div>
-                    
-                    <div class="text-center">
-                        <input type="submit" name="ingresar" class="btn btn-primary height-grande width-grande" value="Iniciar sesi&#243;n">
-                    </div>
-                    <p class="text-center margin-vertical"><a href="" class="color-primary">¿Olvidaste tu contraseña?</a></p>
-                    <hr class="linea">
-
-                </form>
-                <div class="text-center margin-top">
-                    <a href="vistas/signup.php"><button class="btn btn-success height-mediano width-mediano">Crear una cuenta</button></a>
+    <div class="container">
+        <div class="row mt-5 mb-3">
+            <div class="col-12 col-md-7 d-flex justify-content-center align-items-center">
+                <div class="mb-2 px-4">
+                    <div class="text-center"><img src="vistas/imagenes/task.png" loading="lazy" class="img-fluid logo text-center" alt="Logo jobs"></div>
+                    <p class="text-22 text-center"><strong>Task</strong> te ayuda a recordar las actividades que tienes que hacer diariamente.</p>
                 </div>
-
             </div>
+            <div class="col-12 col-md-5">
+                <div class="card mb-2 border-0 shadow">
+                    <h3 class="text-center fw-bold">Iniciar sesi&#243;n</h3>
+                    <form action="vistas/index.php" method="POST" id="formulario">
+                        <div class="mb-3 mt-4">
+                            <input type="text" name="usuario" id="usuario" class="form-control height-grande" maxlength="30" value="<?php if (isset($usuario)) {echo $usuario;} ?>" placeholder="Correo electr&#243;nico" autofocus>
+                            <div>
+                                <p class="mensaje__input-error" id="mensaje-error">El correo es obligatorio</p>
+                            </div>
+                        </div>
+                        <div class="mt-4">
+                            <input type="password" name="password" id="password" class="input form-control height-grande" maxlength="30" value="<?php if (isset($password)) {echo $password;} ?>" placeholder="Contraseña">
+                            <i class="eye fas fa-eye-slash" id="icono"></i>
+                            <div>
+                                <p class="mensaje__input-error" id="mensaje-error2">La contrase&#241;a es obligatoria</p>
+                            </div>
+                        </div>
 
+                        <div class="text-center">
+                            <input type="submit" name="ingresar" class="btn btn-primary text-10 text-white height-grande w-75 btn-lg" value="Iniciar sesi&#243;n">
+                        </div>
+                        <p class="text-center mb-1 mt-2"><a href="" class="text-primary">Olvid&#233; mi contrase&#241;a</a></p>
+                        <hr class="linea">
+                    </form>
+                    <div class="text-center">
+                        <a href="vistas/signup.php"><button class="btn btn-success height-grande w-75 btn-lg">Crear una cuenta</button></a>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-        <?php
-            include("vistas/footer.php");
-        ?>
     </div>
+    
+    <?php
+    include("vistas/footer.php");
+    ?>
+
     <script src="vistas/js/validacion.js"></script>
 </body>
+
 </html>
--- vistas/footer.php
+++ vistas/footer.php
@@ -1,17 +1,28 @@
-        <footer class="card">
-            <div>
-                <!-- https://acortar.link/nH5gzL -->
-                <p>Hecho por <a href="http://jhoncamargo.000webhostapp.com" target="_blank" class="footer-terminos">Jhon Camargo</a></p>
-                <p>Al utilizar este sitio, acepta los <a href="terminos.php" class="footer-terminos">t&#233;rminos de uso</a>.</p>
-            </div>
-            <div>
-                <a href="https://www.facebook.com/jhonalex.camargo.902/" target="_blanck"><i class="fab icono fa-facebook"></i></a>
-                <a href=""><i class="fab icono fa-instagram" target="_blanck"></i></a>
-                <a href=""><i class="fab icono fa-twitter" target="_blanck"></i></a>
-                <a href="https://github.com/JhonCamargo07" target="_blanck"><i class="fab icono fa-github"></i></a>
-            </div>
-            <div>
-                <p>Todos los derechos reservados | Copyright <i class="far fa-copyright"></i></p>
-                <p class="text-center">Versi&#243;n 1.2</p>
+        <footer class="card border-0 mt-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 d-flex justify-content-center">
+                        <div>
+                            <!-- https://acortar.link/nH5gzL -->
+                            <p>Hecho por <a href="http://jhoncamargo.000webhostapp.com" target="_blank" class="footer-terminos">Jhon Camargo</a></p>
+                            <p>Al utilizar este sitio, acepta los <a href="terminos.php" class="footer-terminos">t&#233;rminos de uso</a>.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 d-flex justify-content-center">
+                        <div>
+                            <a href="https://www.facebook.com/jhonalex.camargo.902/" target="_blanck"><i class="text-primary fab icono fa-facebook"></i></a>
+                            <a href=""><i class="text-primary fab icono fa-instagram" target="_blanck"></i></a>
+                            <a href=""><i class="text-primary fab icono fa-twitter" target="_blanck"></i></a>
+                            <a href="https://github.com/JhonCamargo07" target="_blanck"><i class="text-primary fab icono fa-github"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 d-flex justify-content-center">
+                        <div>
+                            <p>Todos los derechos reservados | Copyright <i class="far fa-copyright"></i></p>
+                            <p class="text-center">Versi&#243;n 1.3</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </footer>
